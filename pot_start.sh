a89705de52b7612662bcec8216c4a47fcf17bd7f
Jomy10 <jonas.vbs4@gmail.com>
2024-10-25 18:39:22 +0200

    new start script ensuring stopping

--- pot_start.sh
+++ pot_start.sh
@@ -2,8 +2,17 @@
 
 POT_NAME=website
 
+exitfn() {
+  trap SIGINT
+  pot stop $POT_NAME
+  exit
+}
+
 # Revert to fresh version of the pot, removing any temp files etc
 pot revert -p $POT_NAME
 
+trap "exitfn" INT
+
 pot start $POT_NAME
-pot stop $POT_NAME
+
+trap SIGINT # restore signal hendling
